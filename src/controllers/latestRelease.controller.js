7bdd196976dc67af4c5b5fa237387c2b26c752ef
Ujjwal Kumar Sittu <ujju.sittu@gmail.com>
2024-03-17 17:13:00 +0530

    api_version_updates

--- src/controllers/latestRelease.controller.js
+++ src/controllers/latestRelease.controller.js
@@ -3,14 +3,14 @@ exports.getLatestReleases = function (req, res, next) {
     latest: {
       android_studio: {
         name: "Android Studio",
-        version: "4.0.2.0",
+        version: "2023.2.1.23",
         download_links: {
           windows:
-            "https://redirector.gvt1.com/edgedl/android/studio/install/4.1.2.0/android-studio-ide-201.7042882-windows.exe",
+            "https://redirector.gvt1.com/edgedl/android/studio/install/2023.2.1.23/android-studio-2023.2.1.23-windows.exe",
           linux:
-            "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/4.1.2.0/android-studio-ide-201.7042882-linux.tar.gz",
+            "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.2.1.23/android-studio-2023.2.1.23-linux.tar.gz",
           macos:
-            "https://redirector.gvt1.com/edgedl/android/studio/install/4.1.2.0/android-studio-ide-201.7042882-mac.dmg",
+            "https://redirector.gvt1.com/edgedl/android/studio/install/2023.2.1.23/android-studio-2023.2.1.23-mac.dmg",
         },
       },
       visual_studio_code: {
@@ -25,11 +25,11 @@ exports.getLatestReleases = function (req, res, next) {
       },
       intellij_idea: {
         name: "IntelliJ IDEA",
-        version: "2020.3.2",
+        version: "2023.3.5",
         download_links: {
-          windows: "https://download.jetbrains.com/idea/ideaIC-2020.3.2.exe",
-          linux: "https://download.jetbrains.com/idea/ideaIC-2020.3.2.tar.gz",
-          macos: "https://download.jetbrains.com/idea/ideaIC-2020.3.2.dmg",
+          windows: "https://download.jetbrains.com/idea/ideaIU-2023.3.5.exe",
+          linux: "https://download.jetbrains.com/idea/ideaIU-2023.3.5.tar.gz",
+          macos: "https://download.jetbrains.com/idea/ideaIU-2023.3.5.dmg",
         },
       },
       scripts: {
@@ -37,11 +37,11 @@ exports.getLatestReleases = function (req, res, next) {
           name: "Append To PATH",
           download_links: {
             windows:
-              "https://gist.githubusercontent.com/YazeedAlKhalaf/fae357096390f54a768f091a35efea7f/raw/ee399fb14b7d11256870d58664761eef5d6c1945/append-to-path.bat",
+              "https://gist.githubusercontent.com/ujjwalsittu/24ab9dc6c4e92bb810d774fad5d5ba67/raw/5c58ca907e1e6776a269c77abf319310ea36152f/append-to-path.bat",
             linux:
-              "https://gist.githubusercontent.com/YazeedAlKhalaf/2e063e344b3f3f4bb99a79c601e17a13/raw/208936d814d2e755962f6a57f0dd1b02a8ef891e/append-to-path.sh",
+              "https://gist.githubusercontent.com/ujjwalsittu/b5a8f23325ffca5a0d89f81eeefc9f62/raw/37fda42febac505b0ca85eb731685171ab8f9617/append-to-path.sh",
             macos:
-              "https://gist.githubusercontent.com/YazeedAlKhalaf/2e063e344b3f3f4bb99a79c601e17a13/raw/208936d814d2e755962f6a57f0dd1b02a8ef891e/append-to-path-zsh.sh",
+              "https://gist.githubusercontent.com/ujjwalsittu/4184123c4ac73cbd537c67061a085cb5/raw/23e8a7a852d39fe75fb22a6b5e7dd60b10b8b72a/append-to-path-zsh.sh",
           },
         },
         dist: {
@@ -49,9 +49,9 @@ exports.getLatestReleases = function (req, res, next) {
           download_links: {
             windows: "",
             linux:
-              "https://gist.githubusercontent.com/YazeedAlKhalaf/d03c9bda0d2e3815b819d0ebccdac2e6/raw/bf357bcf5f367e9794458aa86687c48c6a596957/dist.sh",
+              "https://gist.githubusercontent.com/ujjwalsittu/f5703a6043fed08a03aac2596c3dfc08/raw/5862f5ffb31734a8234a8bf4109d7c3901764f3c/dist-linux.sh",
             macos:
-              "https://gist.githubusercontent.com/YazeedAlKhalaf/d03c9bda0d2e3815b819d0ebccdac2e6/raw/bf357bcf5f367e9794458aa86687c48c6a596957/dist.sh",
+              "https://gist.githubusercontent.com/ujjwalsittu/75b440e4fb69f952c2c1006e32142a2f/raw/5dba2eb8b1f486bbec896298f7d43b3c14ec6ce9/dist-macos.sh",
           },
         },
       },
